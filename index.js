865b853b44f8e9743463fb561370164a9bceb46f
DharaVivek <95916493+DharaVivek@users.noreply.github.com>
2023-01-15 03:01:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 var image1=document.querySelectorAll("img")[0]
-var img1src="/images/dice"+randomNumber1+".png";
+var img1src="images/dice"+randomNumber1+".png";
 image1.setAttribute("src",img1src);
 var image2=document.querySelectorAll("img")[1]
-image2.setAttribute("src","/images/dice"+randomNumber2+".png");
+image2.setAttribute("src","images/dice"+randomNumber2+".png");
 if(randomNumber1>randomNumber2)
 document.querySelector("h1").innerHTML="p1 wins";
 else if(randomNumber1<randomNumber2)
